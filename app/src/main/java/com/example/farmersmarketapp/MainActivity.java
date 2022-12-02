e4f85c808339f77c9b857bebd983a28d65b110d5
Antonio Vazquez-Mackay <73569624+chunkychund@users.noreply.github.com>
2022-12-01 16:56:06 -0800

    Added location permission popup

--- app/src/main/java/com/example/farmersmarketapp/MainActivity.java
+++ app/src/main/java/com/example/farmersmarketapp/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.farmersmarketapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,9 +25,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
+        // Create a list of items and add them to the list view
+        // TODO: populate the list with more products
         ListView listView = findViewById(R.id.listView);
-
-        // Populate the list with some grocery items
         List<String> arrList = new ArrayList<>();
         arrList.add("Apples");
         arrList.add("Bananas");
@@ -29,9 +37,24 @@ public class MainActivity extends AppCompatActivity {
         arrList.add("Potatoes");
         arrList.add("Tomatoes");
 
-        // Creating an array adapter
+        // Set the list view to display the items in the list
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
                 this, android.R.layout.simple_list_item_1, arrList );
         listView.setAdapter(arrayAdapter);
+
+        // request location popup
+        Button configButton = findViewById(R.id.button);
+        TextView location = findViewById(R.id.location);
+        configButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                location.setText("Kelowna, BC");
+                // check if the app has permission to access the location
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // if the app does not have permission, request permission
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+                }
+            }
+        });
     }
 }
