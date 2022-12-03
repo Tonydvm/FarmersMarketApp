a4de5ffc61fb9f5a1d6be0f620229087a9ce15ed
Antonio Vazquez-Mackay <73569624+chunkychund@users.noreply.github.com>
2022-12-02 18:14:40 -0800

    ListView now populates from data.txt

--- app/src/main/java/com/example/farmersmarketapp/MainActivity.java
+++ app/src/main/java/com/example/farmersmarketapp/MainActivity.java
@@ -15,7 +15,12 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,14 +32,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
-        LinearLayout linearLayout3 = (LinearLayout) findViewById(R.id.linearlayout3);
-        linearLayout3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO: Add code to handle the click --- presumably this will go to a chat screen
-                // For now, chat screen can just be a screenshot of the chat screen
+        // populate listview from data.txt file, seperated by commas and new lines
+//        writeData(this.findViewById(android.R.id.content));
+
+        String fileName = "data.txt";
+        List<String> list = new ArrayList<String>();
+        try {
+            FileInputStream fis = openFileInput(fileName);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+
+            // populate listview with data from file
+            String line;
+            while ((line = br.readLine()) != null) {
+                list.add(line);
             }
-        });
+            br.close();
+            isr.close();
+            fis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Error reading file", Toast.LENGTH_SHORT).show();
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        ListView listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(adapter);
 
         // request location popup
         Button configButton = findViewById(R.id.button);
@@ -50,5 +72,19 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+    // write test data to data.txt file
+    public void writeData(View view) {
+        String fileName = "data.txt";
+        String data = "Apples, 2.99, 5\nOranges, 3.99, 10\nBananas, 1.99, 15";
+        try {
+            FileOutputStream fos = openFileOutput(fileName, MODE_PRIVATE);
+            fos.write(data.getBytes());
+            fos.close();
+            Toast.makeText(this, "Data written to file", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Error writing to file", Toast.LENGTH_SHORT).show();
+        }
     }
 }
